245d5224ab3abaf2245028e460ad7d4b5c8fba17
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-08 20:44:24 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,8 +3,8 @@ id INTEGER PRIMARY KEY,
 title TEXT,
 category TEXT,
 funding_goal INTEGER,
-start_date INTEGER,
-end_date INTEGER);
+start_date REAL,
+end_date REAL);
 
 CREATE TABLE users (
 id INTEGER PRIMARY KEY,
