dfc2719d83be06d33d22c7ce5d672194d9090d2e
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-08 20:40:29 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,21 @@
 INSERT INTO users (name, age) VALUES ("Bob", 25);
 INSERT INTO users (name, age) VALUES ("Jon", 35);
+INSERT INTO users (name, age) VALUES ("Jen", 45);
+INSERT INTO users (name, age) VALUES ("Jill", 37);
+INSERT INTO users (name, age) VALUES ("Cole", 20);
+INSERT INTO users (name, age) VALUES ("Jin", 39);
+INSERT INTO users (name, age) VALUES ("Len", 55);
+INSERT INTO users (name, age) VALUES ("Keith", 57);
+INSERT INTO users (name, age) VALUES ("Tom", 25);
+INSERT INTO users (name, age) VALUES ("Tina", 35);
+INSERT INTO users (name, age) VALUES ("Lester", 45);
+INSERT INTO users (name, age) VALUES ("Edith", 37);
+INSERT INTO users (name, age) VALUES ("Ted", 20);
+INSERT INTO users (name, age) VALUES ("Sara", 39);
+INSERT INTO users (name, age) VALUES ("Joe", 55);
+INSERT INTO users (name, age) VALUES ("Chris", 57);
+INSERT INTO users (name, age) VALUES ("Jed", 20);
+INSERT INTO users (name, age) VALUES ("Scott", 39);
+INSERT INTO users (name, age) VALUES ("Ken", 55);
+INSERT INTO users (name, age) VALUES ("Odi", 57);
+
